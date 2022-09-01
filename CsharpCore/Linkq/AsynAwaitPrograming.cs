18803d315fc3799f56cd1b3d173c9a33625b8bcb
mkpbd <mostofakamal1990@gmail.com>
2022-09-01 16:20:21 +0600

    async await implementation

---
+++ CsharpCore/Linkq/AsynAwaitPrograming.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpCore.Linkq
+{
+    internal class AsynAwaitPrograming
+    {
+
+      public async  Task<int> GetPrimesCountAsync(int start, int count)
+        {
+            return await Task.Run(() =>
+            ParallelEnumerable.Range(start, count).Count(n =>
+            Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)));
+        }
+
+        async Task Go()
+        {
+            await PrintAnswerToLife();
+            Console.WriteLine("Done");
+        }
+        async Task PrintAnswerToLife()
+        {
+            int answer = await GetAnswerToLife();
+            Console.WriteLine(answer);
+        }
+        async Task<int> GetAnswerToLife()
+        {
+            await Task.Delay(5000);
+            int answer = 21 * 2;
+            return answer;
+        }
+    }
+}
--- CsharpCore/Program.cs
+++ CsharpCore/Program.cs
@@ -10,3 +10,6 @@ new Thread(() =>
     Console.WriteLine("I'm running on another thread!");
     Console.WriteLine("This is so easy!");
 }).Start();
+
+AsynAwaitPrograming asynAwaitPrograming = new AsynAwaitPrograming();
+Console.WriteLine(asynAwaitPrograming.GetPrimesCountAsync(5,50) + "");
