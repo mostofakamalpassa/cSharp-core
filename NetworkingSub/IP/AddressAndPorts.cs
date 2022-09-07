97f33983a6f18e344c8828c7a33ed49718e4b897
mkpbd <mostofakamal1990@gmail.com>
2022-09-07 12:54:55 +0600

    Networking

---
+++ NetworkingSub/IP/AddressAndPorts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkingSub.IP
+{
+    internal class AddressAndPorts
+    {
+
+        public void IPAddressAdd()
+        {
+            IPAddress iPAddress =  new IPAddress(new byte[] {192,168,0,122});
+            IPAddress a2 = IPAddress.Parse("192.168.0.122");
+
+            Console.WriteLine(iPAddress.Equals(a2));
+            Console.WriteLine(iPAddress.AddressFamily);
+        }
+    }
+}
---
+++ NetworkingSub/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NetworkingSub.IP;
+
+AddressAndPorts addressAndPorts = new AddressAndPorts();
+
+addressAndPorts.IPAddressAdd();
